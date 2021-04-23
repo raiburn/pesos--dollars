ed439f1ccf1be06a4b08cb788d41b36b09356876
oscar eduardo ruiz plascencia <oscar_edu_91@hotmail.com>
2021-04-23 12:52:31 -0700

    terminado

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Alert, TouchableHighlight} from 'react-native';
+import Input from './components/Inputs';
 
 export default function App() {
+
+  const A = () =>{
+    alert('21.55');
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableHighlight onPress={A}>
+        <Image source={{
+            uri: 'https://reactnative.dev/img/tiny_logo.png',
+          }}
+          style={{
+            width: 50,
+            height: 50,
+          }}
+          />
+        </TouchableHighlight>
+      <Input />
+      <StatusBar barStyle="dark-content" backgroundColor="#ff0000" hidden={false}/>
     </View>
   );
 }
--- components/Inputs.jsx
+++ components/Inputs.jsx
@@ -1,15 +1,31 @@
 import React, { useState } from "react";
-import { StyleSheet, ScrollView, TextInput} from 'react-native';
-import Button from './Button';
+import { StyleSheet, ScrollView, View, Text, TextInput, Button} from 'react-native';
 
 const Input = () => {
     const [pesos, setPesos] = useState(" ");
     const [dollars, setDollars] = useState(" ");
 
+    const convert = () =>{
+        setDollars(pesos/21.55);
+    }
+
     return (
         <View>
-            <TextInput onChangeText={setPesos} value={pesos}/>
-            <TexInput value={dollars}/>
+            <Text>Pesos</Text>
+            <TextInput style={styles.input} onChangeText={setPesos} value={pesos}/>
+            <Text>Dollars</Text>
+            <TextInput style={styles.input} value={dollars}/>
+            <Button onPress={convert} title="Convertir" color="#841584"/>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    input: {
+      height: 40,
+      margin: 12,
+      borderWidth: 1,
+    },
+  });
+
+export default Input;
